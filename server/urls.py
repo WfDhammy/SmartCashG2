e9dc1ac6324890b9e5a2032c747888fb83ee4b01
WfDhammy <abimbolaokeyedun919@gmail.com>
2025-03-13 08:33:01 -0500

    restored static/media config in settings.py & urls.py

--- server/urls.py
+++ server/urls.py
@@ -30,3 +30,7 @@ urlpatterns = [
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
 
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
