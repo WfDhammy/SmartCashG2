11e407688438fb96b7578986d0494fb187dc3299
WfDhammy <abimbolaokeyedun919@gmail.com>
2025-03-27 10:28:53 -0500

    bamidele

--- wallet/urls.py
+++ wallet/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('<uuid:id>/', views.WalletDetailAPIView.as_view(), name="Detail"),
     path('fund/', views.FundWallet.as_view(), name="fund"),
     path('fund/all', views.FundWalletView.as_view(), name="createWalletList"),
-    path('fund/<uuid:id>/', views.RetrieveFunding.as_view(), name="Detail"),
+    path('<uuid:id>/', views.RetrieveFunding.as_view(), name="Detail"),
     path('webhook/', views.WebHook.as_view(), name="webhook"),
     path('transfer/', views.TransferAction.as_view(), name="transfer"),
 ]
